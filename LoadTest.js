f30a09cc5365d77445edc24331a36dc289345f2f
Hala <halahanaqtah@gmail.com>
2026-02-09 23:07:02 +0300

    Load Test

--- LoadTest.js
+++ LoadTest.js
@@ -15,7 +15,7 @@ export let options = {
     http_req_failed: ['rate<0.1'],       // < 1% errors
     http_req_duration: [
       'p(90)<450',                        // 90% < 450ms
-      'p(95)<600',                        // 95% < 500ms
+      'p(95)<600',                        // 95% < 600ms
     ],
   },
 };
