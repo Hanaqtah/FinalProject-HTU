cd573f08ae868672809297f33e5ebd0cfb601a3a
Hala <halahanaqtah@gmail.com>
2026-02-14 17:45:53 +0300

    Smoke Test Script

--- SmokeTest.js
+++ SmokeTest.js
@@ -15,7 +15,7 @@ export let options = {
 	thresholds: { 
     http_req_failed: ['rate<0.01'],      
     http_req_duration: [
-      'p(90)<300',                        
+      'p(90)<350',                        
       'p(95)<400',                        
     ],
   },
